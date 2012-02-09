8778ec53a61ea63c9657b32286403f88f1ee2257
Wes McKinney <wesmckinn@gmail.com>
2012-02-09 18:21:57 -0500

    try to fix html title

--- conf.py
+++ conf.py
@@ -110,7 +110,7 @@ html_style = 'pydata.css'
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-#html_title = None
+html_title = 'pandas: Python Data Analysis Library'
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 #html_short_title = None
